a3ef0b6921e2b30098da8e0208ac4685a63707ce
MrBlueWhale <15775837993@163.com>
2021-04-08 22:55:28 +0800

    1-6 modify 启动文案

--- src/main/java/com/ibegu/dalao/DalaoApplication.java
+++ src/main/java/com/ibegu/dalao/DalaoApplication.java
@@ -1,13 +1,27 @@
 package com.ibegu.dalao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DalaoApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DalaoApplication.class);
+
+
     public static void main(String[] args) {
-        SpringApplication.run(DalaoApplication.class, args);
+
+        // SpringApplication.run(DalaoApplication.class, args);
+
+        SpringApplication app = new SpringApplication(DalaoApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
+
     }
 
 }
