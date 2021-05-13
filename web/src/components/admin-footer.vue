fa0163f61ed804920234b4157a32f76cf819fec6
MrBlueWhale <15775837993@163.com>
2021-05-13 23:19:02 +0800

    2-6 创建管理员首页admin-home.vue 修改路由 在App.vue中引入 2-7 提取admin-header及admin-footer组件

---
+++ web/src/components/admin-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    ibegu-来个大佬一站式组队平台 @2021
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'admin-footer',
+  props: {
+    msg: String,
+  },
+});
+</script>
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import AdminHome from '../views/admin/admin-home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -14,7 +15,14 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/admin/home',
+    name: 'AdminHome',
+    component: AdminHome
+  },
+
+
 ]
 
 const router = createRouter({
