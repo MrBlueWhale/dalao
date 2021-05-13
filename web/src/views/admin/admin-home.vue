b62aff41a188ecab612fc46d9657e6e0bf09e0fd
MrBlueWhale <15775837993@163.com>
2021-05-14 01:12:58 +0800

    2-12 列表展示并集成图标组件，      npm install --save @ant-design/icons-vue --save

--- web/src/views/admin/admin-home.vue
+++ web/src/views/admin/admin-home.vue
@@ -57,14 +57,51 @@
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
           Content:<br/>
+
+
+          <a-list item-layout="vertical" size="large" :pagination="pagination" :grid="{ gutter: 20, column: 3 }" :data-source="listData">
+            <template #footer>
+              <div>
+                <b>ant design vue</b>
+                footer part
+              </div>
+            </template>
+            <template #renderItem="{ item }">
+              <a-list-item key="item.title">
+                <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+                </template>
+                <template #extra>
+                  <img
+                      width="272"
+                      alt="logo"
+                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                  />
+                </template>
+                <a-list-item-meta :description="item.description">
+                  <template #title>
+                    <a :href="item.href">{{ item.title }}</a>
+                  </template>
+                  <template #avatar><a-avatar :src="item.avatar" /></template>
+                </a-list-item-meta>
+                {{ item.content }}
+              </a-list-item>
+            </template>
+          </a-list>
+
+
+
           通过ref():
           <pre>{{ demos }}</pre>
           <br/>
-          通过reactive():<br/>
-          <pre>
-          {{ demos_reactive }}
-        </pre>
-          <br/>
+<!--          通过reactive():<br/>-->
+<!--          <pre>-->
+<!--          {{ demos_reactive }}-->
+<!--        </pre>-->
+<!--          <br/>-->
 
           <!--        {{demos}}-->
         </a-layout-content>
@@ -82,6 +119,24 @@ import AdminFooter from '@/components/admin-footer.vue';
 import axios from 'axios';
 
 // import HelloWorld from "@/components/HelloWorld.vue";
+
+// const listData: Record<string, string>[] = [];
+const listData: any = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
+
+
 
 export default defineComponent({
   name: 'AdminHome',
@@ -110,9 +165,20 @@ export default defineComponent({
     //html代码要拿到响应式变量 需要在setup的最后return
     return {
       demos,
-      demos_reactive: toRef(demos2, "demos")
+      demos_reactive: toRef(demos2, "demos"),
+      listData,
+      pagination : {
+        onChange: (page: any) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions: [
+        { type: 'StarOutlined', text: '156' },
+        { type: 'LikeOutlined', text: '156' },
+        { type: 'MessageOutlined', text: '2' },
+      ],
     }
-
   },
 
 
@@ -128,10 +194,10 @@ export default defineComponent({
 
 
 <style scoped>
-img {
-  width: 50px;
-  height: 50px;
-}
+/*img {*/
+/*  width: 50px;*/
+/*  height: 50px;*/
+/*}*/
 </style>
 
 
