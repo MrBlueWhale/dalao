5130fca852fe530763079cde3ddfcb8695200029
MrBlueWhale <15775837993@163.com>
2021-05-19 21:35:04 +0800

    l-3-6 管理员管理主办方接口测试 前端关闭    'vue/no-unused-vars': 0,'@typescript-eslint/explicit -module-boundary-types': 0,两个规则校验

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
     'vue/no-unused-components': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'vue/no-unused-vars': 0,
+    '@typescript-eslint/explicit -module-boundary-types': 0,
 
   }
 }
