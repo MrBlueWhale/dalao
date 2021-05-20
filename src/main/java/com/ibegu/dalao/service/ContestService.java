8029878b0ceccf6a1fdba8484ace3a58dad513aa
MrBlueWhale <15775837993@163.com>
2021-05-20 16:23:26 +0800

    z-5 pull处理

--- src/main/java/com/ibegu/dalao/service/ContestService.java
+++ src/main/java/com/ibegu/dalao/service/ContestService.java
@@ -1,42 +1,72 @@
 package com.ibegu.dalao.service;
 
 import ch.qos.logback.core.joran.util.beans.BeanUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.ibegu.dalao.domain.Contest;
 import com.ibegu.dalao.domain.ContestExample;
 import com.ibegu.dalao.mapper.ContestMapper;
 import com.ibegu.dalao.req.ContestReq;
+import com.ibegu.dalao.req.PageReq;
 import com.ibegu.dalao.resp.ContestResp;
-import org.springframework.beans.BeanUtils;
+import com.ibegu.dalao.resp.PageResp;
+import com.ibegu.dalao.utils.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
+import javax.naming.ldap.PagedResultsResponseControl;
 import java.util.List;
 
 
 @Service
 public class ContestService {
 
+    //日志
+
+    private static final Logger LOG = LoggerFactory.getLogger(ContestService.class);
     // @Autowired
     @Resource
     private ContestMapper contestMapper;
 
-    public List<ContestResp> list(ContestReq req) {
+    public PageResp<ContestResp> list(ContestReq req) {
+
         ContestExample contestExample = new ContestExample();
         ContestExample.Criteria criteria = contestExample.createCriteria();
-        criteria.andContestNameLike("%"+req.getContestName()+"%");
-        List<Contest> contestList = contestMapper.selectByExample(contestExample);
-
-        //使用for循环将contest中的值赋给contestResp
-        List<ContestResp> respList = new ArrayList<>();
-        for (Contest contest : contestList) {
-            ContestResp contestResp = new ContestResp();
-            BeanUtils.copyProperties(contest,contestResp);
-            //contestResp.setType("个人赛");
-            respList.add(contestResp);
-
+        if(!ObjectUtils.isEmpty(req.getContestName())){
+            criteria.andContestNameLike("%"+req.getContestName()+"%");
         }
-        return respList;
+
+        //进行分页，通过前端获得的数据来确定请求的每页行数和第几页
+        PageHelper.startPage(req.getPage(),req.getSize());
+        List<Contest> contestList = contestMapper.selectByExample(contestExample);
+        //打印总行数和总页数
+        PageInfo<Contest> pageInfo = new PageInfo<>(contestList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数:{}", pageInfo.getPages());
+        //使用for循环将contest中的值赋给contestResp
+//        List<ContestResp> respList = new ArrayList<>();
+//        for (Contest contest : contestList) {
+////            ContestResp contestResp = new ContestResp();
+////            BeanUtils.copyProperties(contest,contestResp);
+            //对象复制
+//            ContestResp contestResp = CopyUtil.copy(contest, ContestResp.class);
+//            respList.add(contestResp);
+//
+//        }
+
+        //与上方被注释掉的代码等价
+        List<ContestResp> list = CopyUtil.copyList(contestList, ContestResp.class);
+
+        PageResp<ContestResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+
+
+
+        return pageResp;
     }
 
 
--- web/src/views/user/user-record.vue
+++ web/src/views/user/user-record.vue
@@ -1,67 +1,139 @@
 <template>
-  <h1>这里是比赛记录页面</h1>
+  <a-layout>
+    <a-layout-content
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '600px' }"
+    >
+      <a-table
+              :columns="columns"
+              :row-key="record => record.id"
+              :data-source="contest"
+              :pagination="pagination"
+              :loading="loading"
+              @change="handleTableChange"
+      >
+        <template v-slot:action="{ text, record }">
+          <a-space size="small">
+            <a-button type="primary" @click="edit(record)">
+              编辑
+            </a-button>
+            <a-button type="danger">
+              删除
+            </a-button>
+          </a-space>
+        </template>
+      </a-table>
+    </a-layout-content>
+  </a-layout>
 </template>
 
-
 <script lang="ts">
-import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
+  import { defineComponent, onMounted, ref } from 'vue';
+  import axios from 'axios';
 
-import axios from 'axios';
+  export default defineComponent({
+    name: 'UserRecord',
+    setup() {
+      const contest = ref();
+      //分页组件
+      const pagination = ref({
+        current: 1,
+        pageSize: 3,
+        total: 0
+      });
+      const loading = ref(false);
 
-// import HelloWorld from "@/components/HelloWorld.vue";
+      const columns = [
+        {
+          title: '比赛名称',
+          dataIndex: 'contestName'
+        },
+        {
+          title: '开始时间',
+          dataIndex: 'competeStartTime'
+        },
+        {
+          title: '结束时间',
+          dataIndex: 'competeEndTime'
+        },
+        {
+          title: '类型',
+          dataIndex: 'type'
+        },
+        {
+          title: '类别',
+          dataIndex: 'category'
+        },
+        {
+          title: '报名费',
+          dataIndex: 'entryFee'
+        },
+        {
+          title: '范围',
+          dataIndex: 'rank'
+        },
+        {
+          title: 'Action',
+          key:'action',
+          slots:{customRender:'action'}
+        }
+      ];
+
+      /**
+       * 数据查询
+       **/
+      const handleQuery = (params: any) => {
+        loading.value = true;
+        // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+        axios.get("/contest/list", {
+          params:{
+            page: params.page,
+            size: params.size
+          }
+        }).then((response) => {
+          loading.value = false;
+          const data = response.data;
+          contest.value = data.content.list;
+            // 重置分页按钮
+            pagination.value.current = params.page;
+          pagination.value.total = data.content.total;
+        });
+      };
+
+      /**
+       * 表格点击页码时触发
+       */
+      const handleTableChange = (pagination: any) => {
+        console.log("看看自带的分页参数都有啥：" + pagination);
+        handleQuery({
+          page: pagination.current,
+          size: pagination.pageSize
+        });
+      };
 
 
-export default defineComponent({
-  name: 'UserHelp',
+      //进入此页面时初始从这里运行进行查询，此时应该显示列表的第一页数据
+      onMounted(()=> {
+        handleQuery({
+          //真正传递到后端的page和size的名字，需要与后端中pageReq中的参数值保持一致，才能完成映射
+          page: 1,
+          size: pagination.value.pageSize
+        });
+      });
 
-  //放一些参数定义，方法定义
-  setup() {
-
-    //html代码要拿到响应式变量 需要在setup的最后return
-    return {
-
+      return {
+        contest,
+        pagination,
+        columns,
+        loading,
+        handleTableChange
+      }
     }
-  },
-
-
-  components: {
-
-  },
-
-});
+  });
 </script>
 
-
-
-
 <style scoped>
-/*img {*/
-/*  width: 50px;*/
-/*  height: 50px;*/
-/*}*/
-</style>
-
-
-
-
-
-
-
-<style>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
-
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-}
+  img {
+    width: 50px;
+    height: 50px;
+  }
 </style>
